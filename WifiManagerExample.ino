610e4099a292111fd2896ff2a5c1fc23af572c2c
Alex Camilo <alex.camilo@gmail.com>
2020-04-14 13:52:56 -0400

    mqtt connection example

--- WifiManagerExample.ino
+++ WifiManagerExample.ino
@@ -1,6 +1,16 @@
 #include <Preferences.h>
 #include <WiFi.h>
 #include <Esp32WifiManager.h>
+#include "EspMQTTClient.h"
+#define ARDUINO_VARIANT "esp32"
+
+EspMQTTClient client(
+  "robomqtt.cs.wpi.edu",  // MQTT Broker server ip
+  "test",   // Can be omitted if not needed
+  "gompei",   // Can be omitted if not needed
+  "ZumoBot"      // Client name that uniquely identify your device
+);
+
 
 //Create a wifi manager
 WifiManager manager;
@@ -13,7 +23,18 @@ void setup() {
 void loop() {
 	// read the serial port for new passwords and maintain connections
 	manager.loop();
+	client.loop();
 	if (manager.getState() == Connected) {
 		// use the Wifi Stack now connected
 	}
+}
+
+
+void onConnectionEstablished() {
+
+  client.subscribe("mytopic/test", [] (const String &payload)  {
+    Serial.println(payload);
+  });
+
+  client.publish("mytopic/test", "This is a message");
 }
